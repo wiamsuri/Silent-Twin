5a63510a5de57110c31c4b3dfb6bd979ebcfff38
Watt Iamsuri <wiamsuri@gmail.com>
2014-07-08 11:40:33 -0700

    add spritebuilder folder

---
+++ Mirror2.spritebuilder/SpriteBuilder Resources/Dot.h
@@ -0,0 +1,13 @@
+//
+//  Dot.h
+//  Mirror2
+//
+//  Created by Watt Iamsuri on 7/8/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Dot : CCNode
+
+@end
---
+++ Mirror2.spritebuilder/SpriteBuilder Resources/Obstacle.h
@@ -0,0 +1,15 @@
+//
+//  Obstacle.h
+//  Mirror2
+//
+//  Created by Watt Iamsuri on 7/8/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Obstacle : CCNode
+
+- (void) setupRan;
+
+@end
